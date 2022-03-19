83e825a96f47a71b8e11baa9c169d17f9d0b1f9d
manubeledo <estudiocut.oficial@gmail.com>
2022-03-19 15:48:17 -0300

    with index

---
+++ pages/components/title.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Uber Clone</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default IndexPage
